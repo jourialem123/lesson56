872bacd2504f713daf552115ffc1de1ea10c7f69
jourialem123 <alemjouri@gmail.com>
2025-05-24 20:54:59 +0300

    updated

--- activity3.py
+++ activity3.py
@@ -9,3 +9,10 @@ def onsquaretime(num):
 onsquaretime(3)
 onsquaretime(5)
 onsquaretime(4)          
+
+#explaining using first example (num=3):-
+#the i loop will happen n times (in this case 3)
+#the j loop will happen n times(3) every i loop which means the j loop will happen 3X3=9 times
+#each time the j loop happens the iteration adds 1 so in the end (iteration=9)
+#i and j will also update each iteration (for example in iteration 2 the i still didn't finish any loops so i=0 and j is in the second loop so j=2)
+#the j restarts every time i increases by one (for example in iteration 4  i=1 because it finished one loop while j=0)
